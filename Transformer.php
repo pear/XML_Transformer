82c496f68701e7aa1c8524377ae7d8200030a26a
Sebastian Bergmann <sebastian@php.net>
2002-09-16 07:02:11 +0000

    Change license from PHP License 2.02 to PHP License 3.00.
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_Transformer/trunk@95873 c90b9560-bf6c-de11-be94-00142212c4b1

--- Transformer.php
+++ Transformer.php
@@ -1,20 +1,17 @@
 <?php
 //
 // +----------------------------------------------------------------------+
-// | PEAR :: XML Transformer                                              |
+// | PEAR :: XML :: Transformer                                           |
 // +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2002 The PHP Group                                |
+// | Copyright (c) 2002 Sebastian Bergmann <sb@sebastian-bergmann.de> and |
+// |                    Kristian Köhntopp <kris@koehntopp.de>.            |
 // +----------------------------------------------------------------------+
-// | This source file is subject to version 2.02 of the PHP license,      |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
+// | This source file is subject to version 3.00 of the PHP License,      |
+// | that is available through the world-wide-web at                      |
+// | http://www.php.net/license/3_0.txt.                                  |
 // | If you did not receive a copy of the PHP license and are unable to   |
 // | obtain it through the world-wide-web, please send a note to          |
 // | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Author: Sebastian Bergmann <sb@sebastian-bergmann.de>                |
-// |         Kristian Köhntopp <kris@koehntopp.de>                        |
 // +----------------------------------------------------------------------+
 //
 // $Id$
