9bdadac9abef670e6573031cdd7d21fc1e881ce1
Sebastian Bergmann <sebastian@php.net>
2002-09-19 20:26:43 +0000

    Forgot header.
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_Transformer/trunk@96190 c90b9560-bf6c-de11-be94-00142212c4b1

--- test.php
+++ test.php
@@ -1,4 +1,21 @@
 <?php
+//
+// +----------------------------------------------------------------------+
+// | PEAR :: XML :: Transformer                                           |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2002 Sebastian Bergmann <sb@sebastian-bergmann.de> and |
+// |                    Kristian Köhntopp <kris@koehntopp.de>.            |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 3.00 of the PHP License,      |
+// | that is available at http://www.php.net/license/3_0.txt.             |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+//
+
 require_once 'PHPUnit/PHPUnit.php';
 require_once 'XML/Transformer/OutputBuffer.php';
 require_once 'XML/Transformer/Namespace.php';
