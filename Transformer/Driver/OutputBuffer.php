ffe80f27eae01ed0b2f8799c0c7c43f41b5b59cd
Sebastian Bergmann <sebastian@php.net>
2004-01-01 10:31:55 +0000

    Happy New Year!
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_Transformer/trunk@147618 c90b9560-bf6c-de11-be94-00142212c4b1

--- Transformer/Driver/OutputBuffer.php
+++ Transformer/Driver/OutputBuffer.php
@@ -3,7 +3,7 @@
 // +---------------------------------------------------------------------------+
 // | PEAR :: XML :: Transformer :: Driver :: OutputBuffer                      |
 // +---------------------------------------------------------------------------+
-// | Copyright (c) 2002-2003 Sebastian Bergmann <sb@sebastian-bergmann.de> and |
+// | Copyright (c) 2002-2004 Sebastian Bergmann <sb@sebastian-bergmann.de> and |
 // |                         Kristian Köhntopp <kris@koehntopp.de>.            |
 // +---------------------------------------------------------------------------+
 // | This source file is subject to version 3.00 of the PHP License,           |
