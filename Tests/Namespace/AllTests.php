b0a6b9570bfea3cd1f284d443c746a94da63169b
Sebastian Bergmann <sebastian@php.net>
2004-02-06 10:20:32 +0000

    Move to PHPUnit 1.0.X testing layout.
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_Transformer/trunk@150618 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Tests/Namespace/AllTests.php
@@ -0,0 +1,46 @@
+<?php
+//
+// +---------------------------------------------------------------------------+
+// | PEAR :: XML :: Transformer                                                |
+// +---------------------------------------------------------------------------+
+// | Copyright (c) 2002-2004 Sebastian Bergmann <sb@sebastian-bergmann.de> and |
+// |                         Kristian Köhntopp <kris@koehntopp.de>.            |
+// +---------------------------------------------------------------------------+
+// | This source file is subject to version 3.00 of the PHP License,           |
+// | that is available at http://www.php.net/license/3_0.txt.                  |
+// | If you did not receive a copy of the PHP license and are unable to        |
+// | obtain it through the world-wide-web, please send a note to               |
+// | license@php.net so we can mail you a copy immediately.                    |
+// +---------------------------------------------------------------------------+
+//
+// $Id$
+//
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'XML_Transformer_Tests_Namespace_AllTests::main');
+}
+
+require_once 'PHPUnit/Framework/TestSuite.php';
+require_once 'PHPUnit/TextUI/TestRunner.php';
+
+class XML_Transformer_Tests_Namespace_AllTests {
+    public static function main() {
+        PHPUnit_TextUI_TestRunner::run(self::suite());
+    }
+
+    public static function suite() {
+        $suite = new PHPUnit_Framework_TestSuite('XML_Transformer_Namespace');
+
+        return $suite;
+    }
+}
+
+if (PHPUnit_MAIN_METHOD == 'XML_Transformer_Tests_Namespace_AllTests::main') {
+    XML_Transformer_Tests_Namespace_AllTests::main();
+}
+
+/*
+ * vim600:  et sw=2 ts=2 fdm=marker
+ * vim<600: et sw=2 ts=2
+ */
+?>
